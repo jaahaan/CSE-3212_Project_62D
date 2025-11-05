50516967326d66a51e25f059cf92c5d79dddea70
Lisa <cse_1912020239@lus.ac.bd>
2025-11-05 14:58:41 +0600

    AppBar, FloatingActionButton, Drawer

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,10 +8,47 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.home),
+        // leading: Icon(Icons.home),
         backgroundColor: Colors.brown,
         foregroundColor: Colors.greenAccent,
         title: Text("My Flutter Project"),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.person)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: "Add",
+        elevation: 50,
+        child: Icon(Icons.add),
+      ),
+      drawer: NavigationDrawer(
+        children: [
+          DrawerHeader(
+            child: UserAccountsDrawerHeader(
+              decoration: BoxDecoration(color: Colors.green),
+              accountName: Text("Name"),
+              accountEmail: Text("Email"),
+            ),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.home),
+            title: Text("HomePage"),
+          ),
+          Divider(),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.person),
+            title: Text("ProfilePage"),
+          ),
+        ],
+      ),
+
+      body: Text(
+        "Hello",
+        style: GoogleFonts.lobster(textStyle: TextStyle(fontSize: 30)),
       ),
     );
   }
