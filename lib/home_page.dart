619b863eb3a98828367dce61311e94de42ef635d
Lisa <cse_1912020239@lus.ac.bd>
2025-11-09 12:17:54 +0600

    Text, Image

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -46,9 +46,27 @@ class HomePage extends StatelessWidget {
         ],
       ),
 
-      body: Text(
-        "Hello",
-        style: GoogleFonts.lobster(textStyle: TextStyle(fontSize: 30)),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Welcome to homepage",
+                style: GoogleFonts.lobster(textStyle: TextStyle(fontSize: 30)),
+              ),
+              Image.asset('assets/images/flutter.png', height: 50),
+              Image.asset(
+                'assets/images/images.jpeg',
+                height: 400,
+                fit: BoxFit.fill,
+              ),
+              Image.network(
+                'https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500',
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
