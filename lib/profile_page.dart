429c73281e629bdf5af766b8867b54b61ccbb52a
Lisa <cse_1912020239@lus.ac.bd>
2025-12-07 20:42:57 +0600

    TextFormField

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -8,6 +8,9 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  TextEditingController nameController = TextEditingController();
+  String name = "";
+  String? errorMsg;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,10 +27,53 @@ class _ProfilePageState extends State<ProfilePage> {
             child: Padding(
               padding: const EdgeInsets.all(20.0),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   TextFormField(
-                    decoration: InputDecoration(border: OutlineInputBorder()),
+                    controller: nameController,
+                    keyboardType: TextInputType.text,
+                    // onChanged: (value) {
+                    //   name = nameController.text;
+                    //   setState(() {});
+                    // },
+                    // obscureText: true,
+                    onFieldSubmitted: (value) {
+                      setState(() {
+                        errorMsg = null;
+                        if (nameController.text.isEmpty) {
+                          errorMsg = "Field is empty";
+                        } else {
+                          name = nameController.text;
+                        }
+                      });
+                    },
+                    decoration: InputDecoration(
+                      errorText: errorMsg,
+                      labelText: "Name",
+                      hintText: "Enter Name...",
+                      prefixIcon: Icon(Icons.person),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.green),
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                      ),
+                    ),
                   ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        errorMsg = null;
+                        if (nameController.text.isEmpty) {
+                          errorMsg = "Field is empty";
+                        } else {
+                          name = nameController.text;
+                        }
+                      });
+                    },
+                    child: Text("Submit"),
+                  ),
+                  SizedBox(height: 20),
+                  Text("Name is: $name"),
                 ],
               ),
             ),
